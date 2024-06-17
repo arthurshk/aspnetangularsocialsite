28311da1b732ec7c394a84a9339e71d6be618f16
Arthur <arthurdragon39@gmail.com>
2024-06-17 11:49:12 -0400

    commit

--- aspnetangularsocialsite.Server/Migrations/20240617154639_Migration2.Designer.cs
+++ aspnetangularsocialsite.Server/Migrations/20240617154639_Migration2.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using aspnetangularsocialsite.Server.Models;
 
@@ -11,9 +12,11 @@ using aspnetangularsocialsite.Server.Models;
 namespace aspnetangularsocialsite.Server.Migrations
 {
     [DbContext(typeof(SiteDbContext))]
-    partial class SiteDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240617154639_Migration2")]
+    partial class Migration2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
