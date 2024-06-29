cc87ddeee141c40cfe579059b2b02ba7fc2a390b
Arthur <arthurdragon39@gmail.com>
2024-06-28 22:44:50 -0400

    commit

--- aspnetangularsocialsite.client/src/environments/environment.prod.ts
+++ aspnetangularsocialsite.client/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   auth0: {
-    domain: 'default-domain',
-    clientId: 'default-client-id'
+    domain: process.env['NG_AUTH0_DOMAIN'] || 'default-domain',
+    clientId: process.env['NG_AUTH0_CLIENT_ID'] || 'default-client-id'
   }
 };
